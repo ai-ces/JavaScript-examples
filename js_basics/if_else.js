edf99dbb26aa9e1cad987ae81a395f51114f5f31
ai-ces <155421638+ai-ces@users.noreply.github.com>
2024-06-18 04:00:41 +0300

    if_else

---
+++ js_basics/if_else.js
@@ -0,0 +1,34 @@
+// Bir sayinin 10-50 arasinda olup olmadigini gosteriniz.
+
+// let sayi1 = 20;
+// if (sayi1 > 10 && sayi1 < 50) {
+//   console.log("sayi 10-50 arasindadir.");
+// }
+// else {
+//   console.log("sayi 10-50 arasinda degildir.");
+// }
+
+// Bir sayinin pozitif tek sayi olup olmadigini gosteriniz.
+
+// let sayi1 = 21;
+// if (sayi1 % 2 == 1 && sayi1 > 0) {
+//   console.log("Sayi pozitif tek sayidir");
+// } else {
+//   console.log("Sayi pozitif tek sayi degildir");
+// }
+
+// x, y, z sayilarinin buyukluk karsilastirmasini yapiniz
+
+let x = 10,
+  y = 20,
+  z = 30;
+
+if (x > y && x > z) {
+  console.log("x en buyuk");
+} else if (y > x && y > z) {
+  console.log("y en buyuk");
+} else if (z > x && z > y) {
+  console.log("z en buyuk");
+} else {
+  console.log("sayilar esit");
+}
